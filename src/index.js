dcdad41a70d25afbf9d20307b52a550db270e5f9
Bruno Basto <bruno.basto@liferay.com>
2020-04-09 15:32:16 -0300

    Exports

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Scanner from './interpreter/Scanner';
+import Parser from './interpreter/Parser';
+import Callable from './interpreter/Callable';
+import Interpreter from './interpreter/Interpreter';
+import RuntimeException from './interpreter/exceptions/RuntimeException';
+
+export {Scanner, Parser, Interpreter, Callable, RuntimeException};
