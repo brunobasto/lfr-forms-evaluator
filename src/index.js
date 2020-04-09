9272a452bf633ca1455bd1aa93d7cc86a301d48c
Bruno Basto <bruno.basto@liferay.com>
2020-04-09 19:06:17 -0300

    Fix import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import Parser from './interpreter/Parser';
 import Callable from './interpreter/Callable';
 import Interpreter from './interpreter/Interpreter';
 import RuntimeException from './interpreter/exceptions/RuntimeException';
-import printAST from './interpreter/util/debug';
+import {printAST} from './interpreter/util/debug';
 
 export {Scanner, Parser, printAST, Interpreter, Callable, RuntimeException};
